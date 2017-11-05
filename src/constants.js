16a5c4427df57e3ddd11a045530c6db9fc383705
Roland Fong <rfblue2@gmail.com>
2017-11-05 01:16:24 -0400

    Use prod url for prod (duh)

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 // Global Constants
 
-export const BASE_URL = 'http://127.0.0.1:5000'
+export const BASE_URL = (__DEV__) ? 'http://127.0.0.1:5000' : 
+                                    'https://ch-doc-viewer.herokuapp.com/'
 export const FILE_URL = BASE_URL + '/files'
 export const KEYWORDGRAPH_URL = BASE_URL + '/keywordgraph'
