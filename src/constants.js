9e24f26b84353e9611d3d6836da9d4a90e2f456c
Roland Fong <rfblue2@gmail.com>
2017-11-05 01:23:03 -0400

    Prevent crash if __DEV__ undefined

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 // Global Constants
 
-export const BASE_URL = (__DEV__) ? 'http://127.0.0.1:5000' : 
-                                    'https://ch-doc-viewer.herokuapp.com/'
+const DEV_URL = 'http://127.0.0.1:5000'
+const PROD_URL = 'https://ch-doc-viewer.herokuapp.com/'
+
+export const BASE_URL = (__DEV__ == null) ? PROD_URL : DEV_URL 
 export const FILE_URL = BASE_URL + '/files'
 export const KEYWORDGRAPH_URL = BASE_URL + '/keywordgraph'
