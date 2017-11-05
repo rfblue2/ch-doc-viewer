cf3c996f6bb675baffaab65921f8f12354340da4
Roland Fong <rfblue2@gmail.com>
2017-11-05 11:09:17 -0500

    Use process.env.NODE_ENV to get prod or dev environment

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,10 @@
-/* global __DEV__ */
+/* global process */
 // Global Constants
 
 const DEV_URL = 'http://127.0.0.1:5000'
 const PROD_URL = 'https://ch-doc-viewer.herokuapp.com/'
+const DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
-export const BASE_URL = (__DEV__ == null) ? PROD_URL : DEV_URL 
+export const BASE_URL = DEV ? DEV_URL : PROD_URL 
 export const FILE_URL = BASE_URL + '/files'
 export const KEYWORDGRAPH_URL = BASE_URL + '/keywordgraph'
