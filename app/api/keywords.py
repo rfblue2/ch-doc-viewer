a774fe262199f7ecc2774ca2b801183e27657a2c
Roland Fong <rfblue2@gmail.com>
2018-01-03 14:17:37 -0500

    API endpoint for keyiword graph 2

--- app/api/keywords.py
+++ app/api/keywords.py
@@ -8,6 +8,7 @@ from database import fs
 from settings import APP_ROOT
 import networkx as nx
 import operator
+import math
 import re
 import os
 
@@ -172,7 +173,7 @@ def create_keyword_graph(files, window, dist, keywords):
         new_graph = nx.Graph()
         new_graph.add_nodes_from(nx.nodes(graph))
         for keyword in keywords:
-            succ_dict = { k:v for k,v in nx.bfs_successors(graph, keyword) }
+            succ_dict = {k: v for k, v in nx.bfs_successors(graph, keyword)}
             succs = [keyword]
             for i in range(0, dist):
                 temp_succs = []
@@ -189,7 +190,7 @@ def create_keyword_graph(files, window, dist, keywords):
         nx.set_node_attributes(new_graph, name='degree', values=degrees)
         zero_degree_nodes = []
         for n in new_graph:
-            if degrees[n] == 0 and not(n in keywords):
+            if degrees[n] == 0 and not (n in keywords):
                 zero_degree_nodes.append(n)
         new_graph.remove_nodes_from(zero_degree_nodes)
         graph = new_graph
@@ -225,7 +226,7 @@ def get_keywords():
     all_args = request.args.to_dict()
     fileids = re.split(',', all_args['file_ids'])
     window = int(all_args.get('window', 2))
-    n = int(all_args.get('n', -1)) # how many keywords to return
+    n = int(all_args.get('n', -1))  # how many keywords to return
 
     files = fs.find({
         '_id': {
@@ -238,6 +239,126 @@ def get_keywords():
     ranks = nx.pagerank(graph)
     n = n if n != -1 else len(ranks)
     ranks = sorted(ranks.items(), key=operator.itemgetter(1), reverse=True)[:n]
-    ranks = list(map(lambda x: { 'word': x[0], 'rank': x[1] }, ranks))
+    ranks = list(map(lambda x: {'word': x[0], 'rank': x[1]}, ranks))
 
     return jsonify(ranks)
+
+
+# score types include 'freq', 'mu', 'mi', 'mi2', 'll', 'z', 'dice', 'log-dice', 't', 'log-ratio', 'min-sensitivity'
+def create_keyword_graph2(texts, d, terms, score_type):
+
+    stopwords = []
+    with open(os.path.join(APP_ROOT, './static/stopwords.txt'), 'r') as stop_file:
+        for w in stop_file:
+            stopwords.append(w[:-1])
+
+    n = 0  # total number of tokens in entire corpus
+    for t in texts:
+        n += len(t)
+
+    term_freqs = {}  # term frequency (term: r1)
+    collocate_freqs = {}  # the words collocated with term (collocate: c1)
+    collocations = {}  # the collocations ((word, collocate) : { O11, O12, O21, O22, E11, E12, E21, E22 })
+
+    # iterate through text to find term instances
+    for text in texts:
+        for i, word in enumerate(text):
+            if word not in terms:
+                continue
+
+            if word in term_freqs:
+                term_freqs[word] += 1
+            else:
+                term_freqs[word] = 1
+
+            neighbors = []  # words within the window near term of interest
+            for j in range(1, d):
+                if i + j < len(text) and text[i+j] not in stopwords and matcher.match(text[i+j]):
+                    neighbors.append(text[i + j])
+                if i - j >= 0 and text[i-j] not in stopwords and matcher.match(text[i-j]):
+                    neighbors.append(text[i - j])
+
+            for w in neighbors:
+                collocate_freqs[w] = 0 # placeholder to hold the collocate
+
+                if (word, w) in collocations:
+                    collocations[(word, w)]['o11'] += 1
+                else:
+                    collocations[(word, w)] = {'o11': 1}
+
+    # iterate through texts again to tally collocate counts
+    for text in texts:
+        for i, word in enumerate(text):
+            if word not in collocate_freqs:
+                continue
+            else:
+                collocate_freqs[word] += 1
+
+    for (term, collocate) in collocations.keys():
+        k = (term, collocate)
+        o11 = collocations[k]['o11']
+        r1 = term_freqs[term]
+        r2 = n - term_freqs[term]
+        c1 = collocate_freqs[collocate]
+        c2 = n - collocate_freqs[collocate]
+        o12 = r1 - o11
+        o21 = c1 - o11
+        o22 = r2 - o21
+        collocations[k].update({
+            'o12': o12,
+            'o21': o21,
+            'o22': o22,
+            'e11': r1 * c1 / n,
+            'e12': r1 * c2 / n,
+            'e21': r2 * c1 / n,
+            'e22': r2 * c2 / n,
+        })
+
+    # construct graph
+    def create_col_graph2(collocates, scorer):
+        g = nx.DiGraph()
+        list(map(lambda t: g.add_node(t, term=True), terms))
+        list(map(lambda c: g.add_node(c, term=False), collocates))
+        list(map(lambda x: g.add_edge(x[0][0], x[0][1], weight=scorer(x[1])), collocations.items()))
+        return g
+
+    scorers = {
+        'freq': lambda x: x['o11'],
+        'mu': lambda x: x['o11'] / x['e11'],
+        'mi': lambda x: math.log2(x['o11'] / x['e11']),
+        'mi2': lambda x: math.log2(x['o11'] ^ 2 / x['e11']),
+        'll': lambda x: 2 * (x['o11'] * math.log(x['o11'] / x['e11']) +
+                             x['o12'] * math.log(x['o12'] / x['e12']) +
+                             x['o21'] * math.log(x['o21'] / x['e21']) +
+                             x['o22'] * math.log(x['o22'] / x['e22'])),
+        'z': lambda x: (x['o11'] - x['e11']) / math.sqrt(x['e11']),
+        'dice': lambda x: (2 * x['o11']) / (x['r1'] + x['c1']),
+        'log-dice': lambda x: 14 + math.log2((2 * x['o11']) / (x['r1'] + x['c1'])),
+        't': lambda x: (x['o11'] - x['e11']) / math.sqrt(x['o11']),
+        'log-ratio': lambda x: math.log2((x['o11'] * x['r2']) / (x['o21'] * x['r2'])),
+        'min-sensitivity': lambda x: min(x['o11'] / x['c1'], x['o11'] / x['r1'])
+    }
+
+    return create_col_graph2(collocate_freqs.keys(), scorers[score_type])
+
+
+@keywords_api.route('/keywordgraph2', methods=['GET'])
+def get_keyword_graph2():
+    # TODO return error when arg missing
+    all_args = request.args.to_dict()
+    fileids = re.split(',', all_args['file_ids'])
+    window = int(all_args.get('window', 2))
+    keywords = re.split(',', all_args.get('keywords', ''))
+    score_type = all_args.get('score_type', 'mi') # default to mutual information
+
+    files = fs.find({
+        '_id': {
+            '$in': list(map(lambda x: ObjectId(x), fileids))
+        }
+    })
+
+    texts = list(map(lambda f: f.read().decode('utf-8'), files))
+
+    graph = create_keyword_graph2(texts, window, keywords, score_type)
+
+    return jsonify(nx.node_link_data(graph))
